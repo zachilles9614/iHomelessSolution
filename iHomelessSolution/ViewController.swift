3552ab5f920b1b7a868ca96e4c272ec6044933e8
Zachary Auer <zauer@siue.edu>
2016-10-22 08:31:33 -0500

    Added features to user interface
    This has more features added to the user interface, and it also has
    some stuff added for core data use.

--- iHomelessSolution/ViewController.swift
+++ iHomelessSolution/ViewController.swift
@@ -10,10 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let user = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        user.setValue("user", forKey: "username")
+        user.setFalue("password", forKey: "password")
+        
+        do {
+            
+            try context.save()
+            
+            print("Saved")
+        } catch {
+            print("There was an error")
+        }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+        } catch {
+            
+            print("Couldn't fetch results")
+            
+        }
         
     }
 
