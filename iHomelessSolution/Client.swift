8194fb85971bff0f101fc3a56b36bd2c4d0a3c52
Zachary Auer <zauer@siue.edu>
2016-10-22 23:08:58 -0500

    This has the csv parsing working for the program

--- iHomelessSolution/Client.swift
+++ iHomelessSolution/Client.swift
@@ -11,14 +11,42 @@ import Foundation
 
 class Client {
     
+    private var _UUID: String!
     private var _firstName: String!
     private var _middleName: String!
     private var _lastName: String!
-    private var _dob: Date!
-    private var _ssn: Int!
-    private var _veteranStatus: Int!
-    private var _userID: Int!
+    private var _nameDataQuality: String!
+    private var _ssn: String!
+    private var _ssnDataQuality: String!
+    private var _dob: String!
+    private var _dobDataQuality: String!
+    private var _amIndAKNative: String!
+    private var _asian: String!
+    private var _black: String!
+    private var _nativeHIOtherPacific: String!
+    private var _white: String!
+    private var _raceNone: String!
+    private var _gender: String!
+    private var _veteranStatus: String!
+    private var _yearEnteredService: String!
+    private var _yearSeparated: String!
+    private var _worldWarII: String!
+    private var _koreanWar: String!
+    private var _vietnamWar: String!
+    private var _desertStorm: String!
+    private var _afghanistanOEF: String!
+    private var _iraqOIF: String!
+    private var _iraqOND: String!
+    private var _otherTheater: String!
+    private var _militaryBranch: String!
+    private var _discharge_status: String!
+    private var _dateCreated: String!
+    private var _dateUpdated: String!
+    private var _userID: String!
     
+    var uuid: String {
+        return _UUID
+    }
     
     var firstName: String {
         return _firstName
@@ -32,25 +60,148 @@ class Client {
         return _lastName
     }
     
-    var dob: Date {
+    var dob: String {
         return _dob
     }
     
-    var ssn: Int {
+    var ssn: String {
         return _ssn
     }
     
-    var userID: Int {
+    var userID: String {
         return _userID
     }
     
-    init(firstName: String, middleName: String, lastName: String, dob: Date, veteranStatus: Int, ssn: Int, userID: Int) {
+    var nameDataQuality: String {
+        return _nameDataQuality
+    }
+    
+    var ssnDataQuality: String {
+        return _ssnDataQuality
+    }
+    
+    var dobDataQuality: String {
+        return _ssnDataQuality
+    }
+    
+    var amIndAKNative: String {
+        return _amIndAKNative
+    }
+    
+    var asian: String {
+        return _asian
+    }
+    
+    
+    var black: String {
+        return _black
+    }
+    
+    var nativeHIOtherPacific: String {
+        return _nativeHIOtherPacific
+    }
+    
+    var white: String {
+        return _white
+    }
+    
+    var raceNone: String {
+        return _raceNone
+    }
+    
+    var gender: String {
+        return _gender
+    }
+    
+    var veteranStatus: String {
+        return _veteranStatus
+    }
+    
+    var yearEnteredService: String {
+        return _yearEnteredService
+    }
+    
+    var yearSeparated: String {
+        return _yearSeparated
+    }
+    
+    var worldWarII: String {
+        return _worldWarII
+    }
+    
+    var koreanWar: String {
+        return _koreanWar
+    }
+    
+    var desertStorm: String {
+        return _desertStorm
+    }
+    
+    var afghanistanOEF: String {
+        return _afghanistanOEF
+    }
+    
+    var iraqOIF: String {
+        return _iraqOIF
+    }
+    
+    var iraqOND: String {
+        return _iraqOND
+    }
+    
+    var otherTheater: String {
+        return _otherTheater
+    }
+    
+    var militaryBranch: String {
+        return _militaryBranch
+    }
+    
+    var dischargeStatus: String {
+        return _discharge_status
+    }
+    
+    var dateCreated: String {
+        return _dateCreated
+    }
+    
+    var dateUpdated: String {
+        return _dateUpdated
+    }
+    
+    
+    init(uuid: String, firstName: String, middleName: String, lastName: String, nameDataQuality: String, dob: String, ssn: String, ssnDataQuality: String, dobDataQuality: String, amIndAKNative: String, asian: String, black: String, nativeHIOtherPacific: String, white: String, raceNone: String, gender: String, veteranStatus: String, yearEnteredService: String, yearSeparated: String, worldWarII: String, koreanWar: String, desertStorm: String, afghanistanOEF: String, iraqOIF: String, iraqOND: String, otherTheater: String, militaryBranch: String, dischargeStatus: String, dateCreated: String, dateUpdated: String, userID: String ) {
+        self._UUID = uuid
         self._firstName = firstName
         self._middleName = middleName
         self._lastName = lastName
-        self._veteranStatus = veteranStatus
-        self._dob = dob
+        self._nameDataQuality = nameDataQuality
         self._ssn = ssn
+        self._ssnDataQuality = ssnDataQuality
+        self._dob = dob
+        self._dobDataQuality = dobDataQuality
+        self._amIndAKNative = amIndAKNative
+        self._asian = asian
+        self._black = black
+        self._nativeHIOtherPacific = nativeHIOtherPacific
+        self._white = white
+        self._raceNone = raceNone
+        self._gender = gender
+        self._veteranStatus = veteranStatus
+        self._userID = userID
+        self._yearEnteredService = yearEnteredService
+        self._yearSeparated = yearSeparated
+        self._worldWarII = worldWarII
+        self._koreanWar = koreanWar
+        self._desertStorm = desertStorm
+        self._afghanistanOEF = afghanistanOEF
+        self._iraqOIF = iraqOIF
+        self._iraqOND = iraqOND
+        self._otherTheater = otherTheater
+        self._militaryBranch = militaryBranch
+        self._discharge_status = dischargeStatus
+        self._dateCreated = dateCreated
+        self._dateUpdated = dateUpdated
         self._userID = userID
     }
     
--- iHomelessSolution/ViewController.swift
+++ iHomelessSolution/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 
 class ViewController: UIViewController {
 
@@ -20,7 +19,58 @@ class ViewController: UIViewController {
     }
     
     func parseClientCSV() {
-    
+        let path = Bundle.main.path(forResource: "Client", ofType: "csv")!
+        
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            var rows = csv.rows
+            
+            for row in rows {
+                let uuid = row["uuid"]
+                let firstName = row["First_Name"]
+                let middleName = row["Middle_Name"]
+                let lastName = row["Last_Name"]
+                let nameDataQuality = row["Name_Data_Quality"]
+                let ssn = row["SSN"]
+                let ssnDataQuality = row["SSNDataQuality"]
+                let dob = row["DOB"]
+                let dobDataQuality = row["DOBDataQuality"]
+                let amIndAKNative = row["AmIndAKNative"]
+                let asian = row["asian"]
+                let black = row["Black"]
+                let nativeHIOtherPacific = row["NativeHIOtherPacific"]
+                let white = row["White"]
+                let raceNone = row["RaceNone"]
+                let gender = row["Gender"]
+                let otherGender = row["OtherGender"]
+                let veteranStatus = row["VeteranStatus"]
+                let yearEnteredService = row["YearEnteredService"]
+                let yearSeparated = row["YearSeparated"]
+                let worldWarII = row["WorldWarII"]
+                let koreanWar = row["KoreanWar"]
+                let vietnamWar = row["VietnameWar"]
+                let desertStorm = row["DesertStorm"]
+                let afghanistanOEF = row["AfghanistanOEF"]
+                let iraqOIF = row["IraqOIF"]
+                let iraqOND = row["IraqOND"]
+                let otherTheaters = row["OtherTheaters"]
+                let militaryBranch = row["MilitaryBranch"]
+                let dischargeStatus = row["Discharge_Status"]
+                let dateCreated = row["Date_Created"]
+                let dateUpdated = row["DateUpdated"]
+                let userID = row["UserID"]
+                
+                
+                let person = Client(uuid: uuid!, firstName: firstName!, middleName: middleName!, lastName: lastName!, nameDataQuality: nameDataQuality!, dob: dob!, ssn: ssn!, ssnDataQuality: ssnDataQuality!, dobDataQuality: dobDataQuality!, amIndAKNative: amIndAKNative!, asian: asian!, black: black!, nativeHIOtherPacific: nativeHIOtherPacific!, white: white!, raceNone: raceNone!, gender: gender!, veteranStatus: veteranStatus!, yearEnteredService: yearEnteredService!, yearSeparated: yearSeparated!, worldWarII: worldWarII!, koreanWar: koreanWar! , desertStorm: desertStorm!, afghanistanOEF: afghanistanOEF!, iraqOIF: iraqOIF!, iraqOND: iraqOND!, otherTheater: otherTheaters!, militaryBranch: militaryBranch!, dischargeStatus: dischargeStatus!, dateCreated: dateCreated!, dateUpdated: dateUpdated!, userID: userID!)
+            }
+            
+        } catch {
+            
+            print(error)
+        
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
